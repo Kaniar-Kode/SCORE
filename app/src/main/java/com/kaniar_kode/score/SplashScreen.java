b524f12eccee7a8874b7bd139d2753b8091a94d1
macOS <noel.christophe.11@gmail.com>
2016-10-20 21:32:19 +0400

    Kaniar Kode - SC[]RE

--- app/src/main/java/com/kaniar_kode/score/SplashScreen.java
+++ app/src/main/java/com/kaniar_kode/score/SplashScreen.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 import android.widget.ImageView;
 
 public class SplashScreen extends AppCompatActivity {
@@ -14,11 +15,13 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+//        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
         Thread timerThread = new Thread() {
             @Override
             public void run() {
                 try {
-                    sleep(10000);
+                    sleep(5000);
                 } catch (InterruptedException e)
                 {
                     e.printStackTrace();
@@ -30,6 +33,8 @@ public class SplashScreen extends AppCompatActivity {
             }
         };
         timerThread.start();
+
+        setContentView(R.layout.activity_splash_screen);
     }
 
     @Override
